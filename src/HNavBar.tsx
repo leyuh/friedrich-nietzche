137c06922b73d06c4ab7cbe9021a4aa22385a948
Claire Devlin <clairebeargoldielox@icloud.com>
2023-02-28 10:27:27 -0800

    begin bio, h nav

---
+++ src/HNavBar.tsx
@@ -0,0 +1,14 @@
+import "./styles/HNavBar.css";
+
+
+export const HNavBar: React.FC = () => {
+    return <div id="h-nav-bar">
+        <h1>NIETZCHE</h1>
+        <ul>
+            <li><a>BIOGRAPHY</a></li>
+            <li><a>PHILOSOPHY</a></li>
+            <li><a>ANALYSIS</a></li>
+            <li><a>CONCLUSION</a></li>
+        </ul>
+    </div>
+}
---
+++ src/pages/Biography.tsx
@@ -0,0 +1,20 @@
+import "../styles/Biography.css";
+
+import clouds from "../images/clouds.jpeg";
+import portrait from "../images/portrait.jpeg";
+
+export const Biography: React.FC = () => {
+    return <div id="about-page" className="page">
+        <img id="about-bg" src={clouds}/>
+
+        <div className="info-div" id="intro-div">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        </div>
+
+        <img id="portrait-img" src={portrait}/>
+
+        <div className="info-div" id="biography-div">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        </div>
+    </div>
+}
